c04824d21e5f2076964d1014a1b16f15655eea3d
Scott Westover <scottwestover2006@gmail.com>
2016-08-22 00:43:20 +0000

    push

--- index.js
+++ index.js
@@ -6,25 +6,6 @@ var ObjectId = require('mongodb').ObjectID;
 var stormpath = require('express-stormpath');
 var app = express();
 
-var yelp = new Yelp({
-  consumer_key: 'gmnAAe1XgNKeGoRV5knGOw',
-  consumer_secret: 'JO-I347HD8VCV3m3c_eDukbbVrE',
-  token: 'StKbQj9MfQOR7VsSOEL8xXplcRfDfgj7',
-  token_secret: 'z-Wf1BeFvcYb5YxFR2Y_wKcgVvw',
-});
-
-var mongoURL = 'mongodb://heroku_tk3dnhh3:jvru6o4r6rhr7kn35oc7l4ksje@ds013456.mlab.com:13456/heroku_tk3dnhh3';
-
-app.use(stormpath.init(app, {
-  website: true,
-    apiKey: {
-      id: '1P3PA4S39F3IL26KBI1R2AWMY', 
-      secret: 'Eg2b+h/zGdfvTPf3DaJWlCGSLGDYKs4hyzPj5EWPtNw'
-    },
- application: {
-   href: 'https://api.stormpath.com/v1/applications/606aCfu9ELSChkl5nH13k0',
- }
-}));
 
 app.set('port', (process.env.PORT || 5000));
 
